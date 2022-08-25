7db2587efeea86b7a8fee1c4c3cf6c7f7b3ec207
xing-aosheng <1617365850@qq.com>
2022-08-25 21:44:12 +0800

    C语言基础知识巩固（六）

---
+++ test_8-26/test_8-26/test.c
@@ -0,0 +1,48 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//求三角形面积
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	float n = 0, m = 0,p=0;
+//	n = a + b + c;
+//	p = 0.5 * n;
+//	m = sqrt(p * (p - a) * (p - b) * (p - c));
+//	printf("circumference=%.2f area=%.2f\n", n, m);
+//	return 0;
+//}
+//温度转换
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	float f, c=0.0;
+//	scanf("%f", &f);
+//	c = (5.0 / 9.0);   // c为float 就必须保证  /  的左右为x.0
+//	c = c * (f - 32);
+//	printf("%.3f\n", c);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int r;
+//	scanf("%d", &r);
+//	float sum = 0.0;
+//	sum = 3.14 * r * r;
+//	printf("%.2f\n", sum);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	float a, b;
+//	scanf("%f%f", &a, &b);
+//	float sum = 0.0;
+//	sum = (a * b) / (a + b);
+//	printf("%.4f\n", sum);
+//	return 0;
+//}
+//
