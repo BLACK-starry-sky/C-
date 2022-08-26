d17d76225caea77e830e4e1dcd82dc2811042f10
xing-aosheng <1617365850@qq.com>
2022-08-26 23:04:15 +0800

    C语言基础知识巩固（续六）

--- test_8-26/test_8-26/test.c
+++ test_8-26/test_8-26/test.c
@@ -45,4 +45,65 @@
 //	printf("%.4f\n", sum);
 //	return 0;
 //}
-//
+// 
+//函数
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char a1[20] = { 0 };
+//	char a2[] = { "helllo world" };
+//	strcpy(a1, a2);
+//	printf("%s\n", a1);
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char a1[] = { "Hello,world" };
+//	memset(a1, 'x', 5); //前五个字符转换
+//	printf("%s\n", a1);
+//	return 0;
+//}
+//当调用函数时
+//实参传递给形参时，形参将是实参的一份临时拷贝
+//所以对形参的修改是不影响实参的
+//#include <stdio.h>
+//int get_max(int a, int b)
+//{
+//	if (a > b)
+//	{
+//		return a;
+//	}
+//	else
+//	{
+//		return b;
+//	}
+//}
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d%d", &a, &b);
+//	int m = get_max(a, b);
+//	printf("%d\n", m);
+//	return 0;
+//}
+#include <stdio.h>
+void swap(int *px,int *py)
+{
+	int t = 0;
+
+	t=*px; //t=a
+	*px=*py; //a=b;
+	*py = t; //b=t
+}
+int main()
+{
+	int a = 0, b = 0;
+	scanf("%d%d", & a, & b);
+	printf("交换前%d %d\n", a, b);
+	swap(&a, &b);
+	printf("交换后%d %d\n", a, b);
+	return 0;
+}
