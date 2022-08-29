168e720e89f0a208066058a5aa4b42c03e563ecc
xing-aosheng <1617365850@qq.com>
2022-08-29 23:46:28 +0800

    C语言递归训练

--- test_8-28/test_8-28/test.c
+++ test_8-28/test_8-28/test.c
@@ -349,33 +349,33 @@
 //	printf("%d\n", sum);
 //	return 0;
 //}
-#include<stdio.h>
-#include<string.h>
-int my_strlen(char* str)
-{
-	int count = 0;
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-	return count;
-}
-void reverse_string(char* str)
-{
-	char tmp = *str;  //1
-	int len = my_strlen(str);
-	*str = *(str + len - 1); 
-	*(str + len - 1) = '\0';// *(str + len - 1)指向的就是g的位置
-	if (strlen(str + 1) >= 2)
-		reverse_string(str + 1);
-	*(str + len - 1) = tmp;
-}
-int main()
-{
-	char arr[] = "abcdefg";
-	reverse_string(arr);
-
-	printf("%s\n", arr);
-	return 0;
-}
+//#include<stdio.h>
+//#include<string.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char* str)
+//{
+//	char tmp = *str;  //1
+//	int len = my_strlen(str);
+//	*str = *(str + len - 1); 
+//	*(str + len - 1) = '\0';// *(str + len - 1)指向的就是g的位置
+//	if (strlen(str + 1) >= 2)
+//		reverse_string(str + 1);
+//	*(str + len - 1) = tmp;
+//}
+//int main()
+//{
+//	char arr[] = "abcdefg";
+//	reverse_string(arr);
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
